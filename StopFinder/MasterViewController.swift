b6d1f3ac90afe9ef25fb7605e3a7f1b9a1336734
Mikol Aspinwall <maaspinwall@goshen.edu>
2018-04-30 23:45:02 -0400

    complete. parses json (finicky, lib dependencies in jSwift and Foundation), make location requests, talk to server, the whole deal.

--- StopFinder/MasterViewController.swift
+++ StopFinder/MasterViewController.swift
@@ -7,16 +7,104 @@
 //
 
 import UIKit
+import Foundation
+import MapKit
+import CoreLocation
+
+struct CoordinatePair : Codable {
+    let lat: Double
+    let lng: Double
+}
+
+struct Tip : Codable {
+    let info: String
+    let rating: Float
+}
+
+struct Geo : Codable {
+    let location : CoordinatePair
+    let viewport : VP
+}
+
+struct VP : Codable {
+    let northeast : CoordinatePair
+    let southwest : CoordinatePair
+}
+
+struct Stop : Codable {
+    let geometry: Geo
+    let icon : String
+    let id : String
+    let name : String
+    let place_id : String
+    let reference : String
+    let scope : String
+    let types : [String]
+    let vicinity : String
+//    let tips: [Tip]
+}
 
 class MasterViewController: UITableViewController {
+    
+    let locationManager = CLLocationManager()
 
+    private func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) -> (lat: Double, lng: Double) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return(0, 0) }
+        return (locValue.latitude, locValue.longitude)
+    }
+    
+    
+    func retrieveStops(lat: Double, lng: Double) -> ([Stop]) {
+        // what do you think this function does?
+        var myURLString = "https://stopfinder.gc.my/stopsnear/"
+        myURLString += String(lat)
+        myURLString += "/"
+        myURLString += String(lng)
+        myURLString += "/1600"
+        guard let myURL = URL(string: myURLString) else {
+            print("Error: \(myURLString) doesn't seem to be a valid URL")
+            return([])
+        }
+        let request = URLRequest(url: NSURL(string: myURLString)! as URL)
+        do {
+            // Perform the request
+            var response: AutoreleasingUnsafeMutablePointer<URLResponse?>? = nil
+            guard let jsonData = try? NSURLConnection.sendSynchronousRequest(request, returning: response) else {
+                return([])
+            }
+            
+            let decoder = JSONDecoder()
+            let stops = try? decoder.decode([Stop].self, from: jsonData)
+            return(stops!)
+        }
+    }
+    
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Int]()
-
-
+    var objects = ["In: College & Main", "Out: College & Main"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        }
+        
+        let stops = retrieveStops(lat: (locationManager.location?.coordinate.latitude)!, lng: (locationManager.location?.coordinate.longitude)!)
+        
+        for stop in stops {
+            objects.append(stop.name)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
         navigationItem.leftBarButtonItem = editButtonItem
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
@@ -39,7 +127,8 @@ class MasterViewController: UITableViewController {
 
     @objc
     func insertNewObject(_ sender: Any) {
-        objects.insert(Int(arc4random_uniform(6)), at: 0)
+        objects.insert(String(Int(arc4random_uniform(6))), at: 0)
+        // inserts new objects (me)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
     }
@@ -93,4 +182,19 @@ class MasterViewController: UITableViewController {
 
 
 }
+
+extension MasterViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) -> (lat: Double, long: Double) {
+        if let lat = locations.last?.coordinate.latitude, let long = locations.last?.coordinate.longitude {
+            return(lat, long)
+        } else {
+            print("No coordinates")
+        }
+        return(0, 0)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
 
